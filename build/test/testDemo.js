03e07cc98c2188ce652e7dac09e3c963db1f14a3
萧萧落雨 <8071489+xluoyu@user.noreply.gitee.com>
2021-08-23 17:21:28 +0800

    up

--- build/test/testDemo.js
+++
@@ -1,6 +0,0 @@
-const demoLoader = require('../loaders/demo-loader')
-const fs = require('fs')
-
-const code = fs.readFileSync('../../src/components/button/demos/color.demo.md', 'utf-8')
-
-demoLoader(code)
